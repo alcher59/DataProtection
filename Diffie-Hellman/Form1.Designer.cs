856ec66974cbfcc6037c0fda371f4f1ef045c8c7
Alexsey <pingwin59@mail.ru>
2020-09-27 18:15:29 +0500

    c

---
+++ Diffie-Hellman/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Diffie_Hellman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox_enc = new System.Windows.Forms.TextBox();
+            this.btn_enc = new System.Windows.Forms.Button();
+            this.textBox_dec = new System.Windows.Forms.TextBox();
+            this.btn_dec = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox_crypt = new System.Windows.Forms.TextBox();
+            this.maskedTextBox_x = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox_y = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.maskedTextBox_y);
+            this.groupBox1.Controls.Add(this.maskedTextBox_x);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(174, 105);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Простые числа";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "x:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "y:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_enc);
+            this.groupBox2.Controls.Add(this.textBox_enc);
+            this.groupBox2.Location = new System.Drawing.Point(208, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(528, 178);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Шифрование";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btn_dec);
+            this.groupBox3.Controls.Add(this.textBox_dec);
+            this.groupBox3.Location = new System.Drawing.Point(208, 196);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(528, 183);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Дешифрование";
+            // 
+            // textBox_enc
+            // 
+            this.textBox_enc.Location = new System.Drawing.Point(7, 20);
+            this.textBox_enc.Multiline = true;
+            this.textBox_enc.Name = "textBox_enc";
+            this.textBox_enc.Size = new System.Drawing.Size(515, 109);
+            this.textBox_enc.TabIndex = 0;
+            // 
+            // btn_enc
+            // 
+            this.btn_enc.Location = new System.Drawing.Point(447, 135);
+            this.btn_enc.Name = "btn_enc";
+            this.btn_enc.Size = new System.Drawing.Size(75, 37);
+            this.btn_enc.TabIndex = 1;
+            this.btn_enc.Text = "OK";
+            this.btn_enc.UseVisualStyleBackColor = true;
+            this.btn_enc.Click += new System.EventHandler(this.btn_enc_Click);
+            // 
+            // textBox_dec
+            // 
+            this.textBox_dec.Location = new System.Drawing.Point(6, 19);
+            this.textBox_dec.Multiline = true;
+            this.textBox_dec.Name = "textBox_dec";
+            this.textBox_dec.Size = new System.Drawing.Size(515, 109);
+            this.textBox_dec.TabIndex = 1;
+            // 
+            // btn_dec
+            // 
+            this.btn_dec.Location = new System.Drawing.Point(446, 134);
+            this.btn_dec.Name = "btn_dec";
+            this.btn_dec.Size = new System.Drawing.Size(75, 37);
+            this.btn_dec.TabIndex = 2;
+            this.btn_dec.Text = "OK";
+            this.btn_dec.UseVisualStyleBackColor = true;
+            this.btn_dec.Click += new System.EventHandler(this.btn_dec_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox_crypt);
+            this.groupBox4.Location = new System.Drawing.Point(12, 124);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(174, 255);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Шифр";
+            // 
+            // textBox_crypt
+            // 
+            this.textBox_crypt.Location = new System.Drawing.Point(7, 20);
+            this.textBox_crypt.Multiline = true;
+            this.textBox_crypt.Name = "textBox_crypt";
+            this.textBox_crypt.ReadOnly = true;
+            this.textBox_crypt.Size = new System.Drawing.Size(161, 229);
+            this.textBox_crypt.TabIndex = 0;
+            // 
+            // maskedTextBox_x
+            // 
+            this.maskedTextBox_x.Location = new System.Drawing.Point(38, 33);
+            this.maskedTextBox_x.Mask = "0000000000000000";
+            this.maskedTextBox_x.Name = "maskedTextBox_x";
+            this.maskedTextBox_x.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox_x.TabIndex = 4;
+            this.maskedTextBox_x.Text = "5";
+            // 
+            // maskedTextBox_y
+            // 
+            this.maskedTextBox_y.Location = new System.Drawing.Point(38, 65);
+            this.maskedTextBox_y.Mask = "0000000000000000";
+            this.maskedTextBox_y.Name = "maskedTextBox_y";
+            this.maskedTextBox_y.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox_y.TabIndex = 4;
+            this.maskedTextBox_y.Text = "7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 387);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_enc;
+        private System.Windows.Forms.TextBox textBox_enc;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btn_dec;
+        private System.Windows.Forms.TextBox textBox_dec;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox_crypt;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox_y;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox_x;
+    }
+}
+
---
+++ Diffie-Hellman/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Numerics;
+
+namespace Diffie_Hellman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //проверка: простое ли число?
+        private bool IsTheNumberSimple(long n)
+        {
+            if (n < 2)
+                return false;
+
+            if (n == 2)
+                return true;
+
+            for (long i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        private int Get_K(long x, long y)
+        {
+            int n = 43, q = 317;
+
+            //int a = (int)(Math.Pow(q, x) % n);
+            int b = (int)(Math.Pow(q, y) % n);
+            int k = (int)(Math.Pow(b, x) % n);
+
+            return k;
+        }
+
+        private void btn_enc_Click(object sender, EventArgs e)
+        {
+            if ((maskedTextBox_x.Text.Length > 0) && (maskedTextBox_y.Text.Length > 0))
+            {
+                long x = Convert.ToInt64(maskedTextBox_x.Text);
+                long y = Convert.ToInt64(maskedTextBox_y.Text);
+
+                if (IsTheNumberSimple(x) && IsTheNumberSimple(y))
+                {
+                    textBox_crypt.Clear();
+                    int k = Get_K(x, y);
+                    string text = textBox_enc.Text.ToLower();
+                    foreach (char i in text.ToCharArray())
+                    {
+                        if (textBox_crypt.Text.Length == 0)
+                            textBox_crypt.Text = (i + k).ToString();
+                        else
+                            textBox_crypt.AppendText("\r\n" + (i + k));
+                    }
+                }
+                else
+                    MessageBox.Show("x или y - не простые числа!");
+            }
+            else
+                MessageBox.Show("Введите x и y!");
+        }
+
+        private void btn_dec_Click(object sender, EventArgs e)
+        {
+            if ((maskedTextBox_x.Text.Length > 0) && (maskedTextBox_y.Text.Length > 0))
+            {
+                long x = Convert.ToInt64(maskedTextBox_x.Text);
+                long y = Convert.ToInt64(maskedTextBox_y.Text);
+
+                string[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+                int k = Get_K(x, y);
+                List<string> input = new List<string>();
+                textBox_dec.Clear();
+                for (int i = 0; i < textBox_crypt.Lines.Length; i++)
+                    input.Add(textBox_crypt.Lines[i]);
+                string res = "";
+                foreach (var str in input)
+                {
+                    for (int i = 97 + k; i <= 124 + k; i++)
+                    {
+                        if (str == i.ToString())
+                            res += alphabet.GetValue(i - 97 - k) ;
+                    }
+                }
+                textBox_dec.Text = res;
+            }
+            else
+                MessageBox.Show("Введите секретный ключ!");
+        }
+    }
+}
