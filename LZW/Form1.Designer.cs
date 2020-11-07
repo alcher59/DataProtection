f8016c81a032da19910653fa80707d560d7577bf
Alexsey <pingwin59@mail.ru>
2020-11-07 15:51:11 +0500

    lzw

---
+++ LZW/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LZW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.TextBox();
+            this.output1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.output2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите текст:";
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(16, 30);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(471, 20);
+            this.input.TabIndex = 1;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // output1
+            // 
+            this.output1.Enabled = false;
+            this.output1.Location = new System.Drawing.Point(244, 19);
+            this.output1.Multiline = true;
+            this.output1.Name = "output1";
+            this.output1.Size = new System.Drawing.Size(222, 38);
+            this.output1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(4, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Сжать с помощью LZW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(4, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сжать с помощью арифметического кодирования";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.TextChanged += new System.EventHandler(this.button2_TextChanged);
+            // 
+            // output2
+            // 
+            this.output2.Enabled = false;
+            this.output2.Location = new System.Drawing.Point(244, 63);
+            this.output2.Multiline = true;
+            this.output2.Name = "output2";
+            this.output2.Size = new System.Drawing.Size(222, 38);
+            this.output2.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.output1);
+            this.groupBox1.Controls.Add(this.output2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 108);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Сжатие";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 196);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "LZW";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox output2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ LZW/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LZW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            input.Text = "abacabadabacabae";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CompressText_LZW();
+        }
+
+        public void CompressText_LZW() //Lempel-Ziv-Welch
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            string inputStr = input.Text;
+
+            string currStr = inputStr.Substring(0, 2);  //текущая строка
+            string curr = "";  //текущий символ
+            string next = ""; //следующий символ
+
+            int num = 0; //номер элемента в словаре
+
+            foreach (var s in input.Text.Distinct())
+            {
+                dict.Add(num, s.ToString());
+                num++;
+            }
+
+            int tempNum = num;
+            for (int i = 0; i < inputStr.Length; i++)
+            {
+                curr = inputStr[i].ToString();
+                if(i == inputStr.Length - 1)
+                {
+                    output1.Text += dict.FirstOrDefault(x => x.Value == curr).Key;
+                }
+                else
+                {
+                    next = inputStr[i + 1].ToString();
+                    if (tempNum < num)
+                    {
+                        currStr = curr + next;
+                        tempNum++;
+                    }
+
+                    if (!dict.Values.Contains(currStr))
+                    {
+                        dict.Add(num, currStr);
+                        num++;
+                        output1.Text += dict.FirstOrDefault(x => x.Value == currStr.Remove(currStr.Length - 1, 1)).Key;
+                        currStr = "";
+                    }
+                    else
+                    {
+                        currStr += inputStr[i + currStr.Length];
+                    }
+                }
+            }
+        }
+
+        public void CompressText_AC() //arithmetic coding
+        {
+
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true ? input.Text != "" : false;
+        }
+
+        private void button2_TextChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = true ? output1.Text != "" : false;
+        }
+    }
+}
