efd813de879d15e38c0e931c88dd4801bff24b04
Alexsey <pingwin59@mail.ru>
2020-11-08 19:43:42 +0500

    c

--- ErrorCorrection/Form1.Designer.cs
+++ ErrorCorrection/Form1.Designer.cs
@@ -33,12 +33,19 @@
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Location = new System.Drawing.Point(16, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(214, 13);
             this.label1.TabIndex = 0;
@@ -47,7 +54,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 82);
+            this.label2.Location = new System.Drawing.Point(16, 130);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(155, 13);
             this.label2.TabIndex = 1;
@@ -55,21 +62,25 @@
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(33, 46);
+            this.maskedTextBox1.Location = new System.Drawing.Point(19, 44);
             this.maskedTextBox1.Name = "maskedTextBox1";
             this.maskedTextBox1.Size = new System.Drawing.Size(211, 20);
             this.maskedTextBox1.TabIndex = 2;
+            this.maskedTextBox1.TextChanged += new System.EventHandler(this.maskedTextBox1_TextChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(33, 98);
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(19, 146);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(211, 20);
             this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(33, 136);
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(19, 70);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(211, 23);
             this.button1.TabIndex = 4;
@@ -77,20 +88,74 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 182);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Кодирование";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(271, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(253, 182);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Декодирование";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(19, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Выполнить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Результат";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(19, 146);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(211, 20);
+            this.textBox2.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(272, 177);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.maskedTextBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(535, 205);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "ErrorCorrection";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -101,6 +166,11 @@
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- ErrorCorrection/Form1.cs
+++ ErrorCorrection/Form1.cs
@@ -13,35 +13,104 @@ namespace ErrorCorrection
     public partial class Form1 : Form
     {
         
-        const int r = 3; //степень
-        const string p = "1011"; // P(x) - порождающий полином при степени r
+        int r = 0; //степень полинома
+        Dictionary<int, string> table = new Dictionary<int, string>(); //таблица порождающих полиномов
+        List<int> polynom_H, polynom_P, polynom_F, polynom_R, polynom_Dev;
 
         public Form1()
         {
             InitializeComponent();
-            maskedTextBox1.Mask = "0000"; 
+           
+            table.Add(2, "111");
+            table.Add(3, "1011");
+            table.Add(4, "10011");
+            table.Add(5, "100101");
+            table.Add(6, "1000011");
+            table.Add(7, "10001001");
+            table.Add(8, "111100111");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var polynomH = GetH(GetPolynom(maskedTextBox1.Text));
-            var polynomDev = PolynomsDevision(polynomH, GetPolynom(p));
-            polynomH.AddRange(polynomDev);
+            textBox1.Enabled = true;
+            r = maskedTextBox1.Text.Length - 1;
+
+            polynom_H = GetMulti(GetPolynom(maskedTextBox1.Text), r); // получим степени полинома h(x) = A(x)*x^r в нужном порядке 
+            polynom_P = GetPolynom(table[r]);
+            polynom_Dev = PolynomsDevision(polynom_H, polynom_P); // получим степени полинома остатка от деления h(x)/P(x) в нужном порядке
+
+            polynom_F = polynom_H;
+            polynom_F.AddRange(polynom_Dev); //результат = h(x) + остаток от деления
+
+            textBox1.Text = GetString(polynom_F);
+        }
+
+        public string GetString(List<int> polynom)
+        {
             string result = "";
-            for(int i = polynomH.Max(); i >= polynomH.Min(); i--)
+            for (int i = polynom.Max(); i >= polynom.Min(); i--)
             {
-                if (polynomH.Contains(i))
+                if (polynom.Contains(i))
                     result += "1";
                 else
                     result += "0";
             }
-            textBox1.Text = result;
+            return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var p1 = GetPolynom(textBox1.Text);
+
+            polynom_R = PolynomsDevision(p1, polynom_P);
+
+            if (polynom_R[0] == 0 && polynom_R.Count == 1)
+            {
+                textBox2.Text = "Ошибки нет.";
+            }
+            else
+            {
+                textBox2.Text = ErrorCorrection(textBox1.Text);
+            }
+        }
+
+        public string ErrorCorrection(string str)
+        {
+            string res = textBox1.Text;
+            string e = "";
+            for (int j = 0; j < str.Length; j++)
+            {
+                if (j == 0) e += "1"; else e += "0";
+            }
+            var polynom_E = GetPolynom(e);
+
+            var polynom_R0 = PolynomsDevision(polynom_E, polynom_P);
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                var polynom_Ri = PolynomsDevision(GetMulti(GetPolynom(textBox1.Text), i), polynom_P);
+
+                if (Enumerable.SequenceEqual(polynom_Ri.OrderBy(t => t), polynom_R0.OrderBy(t => t))) //равны ли полиномы
+                {
+                    char element = res.ElementAt(i);
+                    if (element == Convert.ToChar("0"))
+                    {
+                        res = res.Remove(i, 1).Insert(i, "1");
+                    }
+                    if (element == Convert.ToChar("1"))
+                    {
+                        res = res.Remove(i, 1).Insert(i, "0");
+                    }
+                }
+            }
+
+            return res;
         }
 
         public List<int> GetPolynom(string str) // получим степени полинома в нужном порядке 
         {
             List<int> degrees = new List<int>();
-            int degree = r;
+            int degree = str.Length - 1;
             foreach(char c in str)
             {
                 if (c == '1')
@@ -51,14 +120,13 @@ namespace ErrorCorrection
             return degrees;
         }
 
-        public List<int> GetH(List<int> degrees) // получим степени полинома h(x) = A(x)*x^r в нужном порядке 
+        public List<int> GetMulti(List<int> polynom, int r) //polynom * r 
         {
-           
-            for(int i = 0; i < degrees.Count; i++)
+            for(int i = 0; i < polynom.Count; i++)
             {
-                degrees[i] += r;
+                polynom[i] += r;
             }
-            return degrees;
+            return polynom;
         }
 
         public List<int> XOR(List<int> a, List<int> b) 
@@ -68,52 +136,60 @@ namespace ErrorCorrection
             return result;
         }
 
-        public List<int> GetElementsFrom(List<int> list)
+        public List<int> PolynomsDevision(List<int> p1, List<int> p2)  //остаток деления полиномов
         {
-            List<int> newList = new List<int>();
-            foreach (var i in list)
-            {
-                newList.Add(i);
-            }
-            return newList;
-        }
-
-        public List<int> PolynomsDevision(List<int> h, List<int> p) // получим степени полинома остатка от деления h(x)/P(x) в нужном порядке
-        {
-            List<int> temp1 = GetElementsFrom(h); //временный список 1 для вычислений
+            List<int> temp1 = p1.ToList(); //временный список 1 для вычислений
             List<int> temp2 = new List<int>(); //временный список 2 для вычислений
+
             List<int> result = new List<int>(); //результат деления
-            List<int> remainder = GetElementsFrom(h); //остаток
+            List<int> remainder = p1.ToList(); //остаток
             int i = 0;
             
-            while (remainder[0] >= p[0])
+            while (remainder[0] >= p2[0])
             {
-                if (remainder[0] == p[0])
+                if (remainder[0] == p2[0])
                 {
                     i++;
                     result.Add(0);
                 }
                 else
                 {
-                    result.Add(remainder[0] - p[0]);
+                    result.Add(remainder[0] - p2[0]);
                 }
                     
-                for (int j = 0; j < p.Count; j++)
+                for (int j = 0; j < p2.Count; j++)
                 {
-                    temp2.Add(result[i] + p[j]);
+                    temp2.Add(result[i] + p2[j]);
                 }
 
-                remainder = XOR(temp1, temp2);
+                remainder = XOR(temp1, temp2).OrderByDescending(x => x).ToList();
                 temp1.Clear();
                 temp2.Clear();
-                temp1 = GetElementsFrom(remainder);
+                temp1 = remainder.ToList();
 
-                if (remainder[0] > p[0])
+                if(remainder.Count == 0)
+                {
+                    remainder.Add(0);
+                    return remainder;
+                }
+
+                if (remainder[0] > p2[0])
                 {
                     i++;
                 }
             } 
             return remainder;
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = true ? textBox1.Text != "" : false;
+        }
+
+        private void maskedTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true ? maskedTextBox1.Text != "" : false;
+        }
+    
     }
 }
